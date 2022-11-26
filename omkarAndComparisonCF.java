7f318ffa2bfb12f9ae52dff2ea7fd4c85930809b
Ayush Mandal <66141678+code4oryou@users.noreply.github.com>
2022-11-26 16:59:21 +0530

    Omkar and Completion - Day 72

---
+++ omkarAndComparisonCF.java
@@ -0,0 +1,20 @@
+// Day 72
+// https://codeforces.com/problemset/problem/1372/A
+// 1372 A. Omkar and Completion
+
+import java.util.*;
+
+public class omkarAndComparisonCF {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            while (n-- > 0) {
+                System.out.print("1 ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
