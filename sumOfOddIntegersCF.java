f36fe67bafe68434e964388caa30b32f8647651e
Ayush Mandal <66141678+code4oryou@users.noreply.github.com>
2022-11-27 20:24:05 +0530

    Sum of Odd Integers - Day 73

---
+++ sumOfOddIntegersCF.java
@@ -0,0 +1,21 @@
+// Day 73
+// https://codeforces.com/problemset/problem/1327/A
+// 1327 A. Sum of Odd integers
+
+import java.util.*;
+
+public class sumOfOddIntegersCF {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int n = in.nextInt();
+            int k = in.nextInt();
+            if (n % 2 == k % 2 && n >= k * k)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+        in.close();
+    }
+}
