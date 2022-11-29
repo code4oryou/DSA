eb2b5cba896c8dade24a7996c49e4ee8ac9d2f42
Ayush Mandal <66141678+code4oryou@users.noreply.github.com>
2022-11-29 23:05:06 +0530

    Raising Bacteria - Day 75

---
+++ raisingBacteriaCF.java
@@ -0,0 +1,14 @@
+// Day - 75
+// https://codeforces.com/problemset/problem/579/A
+// 579 A. Raising Bacteria
+
+import java.util.*;
+
+public class raisingBacteriaCF {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(Integer.bitCount(n));
+        in.close();
+    }
+}
